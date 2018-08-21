dfb75e03b13f083d87cc37000e3e510b01f88c4b
axa <ivan@aksentijevic.net>
2018-08-21 19:33:22 +0200

    more readable alternative, using more of igraph api

--- main.py
+++ main.py
@@ -3,31 +3,28 @@ from igraph import *
 
 terms = []
 descriptions = []
+# initialize graph 
+g = Graph(directed=True)
 
-# just opneing file
+# in, one pass - read from file, build terms index and add edges to graph 
 with open('data.csv') as f:
     r = csv.reader(f)
-    for row in r:
+    for i, row in enumerate(r):
         # we read each line, and place values in two arrays - terms and descriptions
         terms.append(row[0])
         descriptions.append(row[1])
+        # adding to graph, first allocating vertices, and then throw an edge between
+        g.add_vertex(name=row[0] + ' desc', label=row[1], type='description')
+        g.add_vertex(name=row[0], label=row[0], type='term')
+        g.add_edge(2*i, 2*i + 1, type='defines')
 
-# this size of dataset
+# naive search through descriptions to establish connections with depeneding terms
 size = len(terms)
-# creating edges - description are pointing to terms
-edges = [(size + i, i) for i in xrange(size)]
-print(edges)
-
-g = Graph(edges, directed=True)
-g.vs['text'] = descriptions + terms
-g.es['type'] = 'defines'
-
-# naive search through descriptions to establish connections with dpeneding terms
 for x, d in enumerate(descriptions):
     for y, t in enumerate(terms):
         # super naive check, as it will pick any match 
-        if t in d:
-            g.add_edge(size + x, y, type='depends')
+        if t.lower() in d.lower():
+            g.add_edge(2*x, 2*y + 1, type='depends')
 
 print(g)
 layout = g.layout("kk")
